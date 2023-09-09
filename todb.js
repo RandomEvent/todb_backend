a2dffa61baa0b43069559ef18b060a2982a7f494
user <user@email.com>
2023-09-09 12:21:33 +0100

    add message

--- todb.js
+++ todb.js
@@ -1,12 +1,13 @@
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const message = process.env.MESSAGE;
 
 const todb = () => {
-    console.log(message)
+    console.log('running...')
+    console.log('Message:', message);
 };
 
 todb();
